13936d93cf3c3694c08b2ee2f2cc9e1e44549979
chanhcs <chanhpm.cs@gmail.com>
2019-09-30 21:20:34 +0700

    --amend

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -6,7 +6,7 @@ const video = () => {
         <div>
             <h3>ღ How to make a perfect Hamburger ღ </h3>
             <div className={classes.Video}>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/talwV3qDM8E" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/talwV3qDM8E" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
             </div>
         </div>
     );
