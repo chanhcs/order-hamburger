56bd3c1a4f60dfebcd68872edc04f11c737e055a
chanhcs <chanhpm.cs@gmail.com>
2019-10-08 21:13:09 +0700

    updating readme.md

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './Video.css';
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 
 const video = () => {
     return (
-        <div>
+        <Auxiliary>
             <h3>ღ How to make a perfect Hamburger ღ </h3>
             <div className={classes.Video}>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/talwV3qDM8E" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
             </div>
-        </div>
+        </Auxiliary>
     );
 }
 
